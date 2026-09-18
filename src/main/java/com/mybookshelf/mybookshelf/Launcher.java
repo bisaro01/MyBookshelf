0729467f54edb5962623b39696c2e26ce76533d6
Lovisa Olsson <lovols@chalmers.se>
2026-09-18 12:57:09 +0200

    Remove base for restructure

--- src/main/java/com/mybookshelf/mybookshelf/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.mybookshelf.mybookshelf;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
